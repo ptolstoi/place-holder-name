0b6eabbe5a19f915fef06b2cf5ee05272e381ca8
Copyed <m.niederreiter@tum.de>
2015-02-23 14:11:25 +0100

    autofade scenes

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public int sceneNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+
+            AutoFade.LoadLevel(sceneNumber, 1, 1, Color.black);
+
+
+        }
+	
+	}
+}
